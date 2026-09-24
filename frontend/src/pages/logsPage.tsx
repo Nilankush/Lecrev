9f4de51d88a22ad1d30825b7c511876230079093
nilankush <dasnilankush28@gmaail.com>
2026-09-24 10:05:12 +0530

    fix: signin box open on logs page

--- frontend/src/pages/logsPage.tsx
+++ frontend/src/pages/logsPage.tsx
@@ -1,12 +1,14 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import Header from "@/components/header";
 import LogBox from "@/components/logBox";
+import SignInDialog from "@/components/signInDialog";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { OpenSignInContext } from "@/context/openSignInContext";
 import axios, { type AxiosResponse } from "axios";
 import { ChevronDown, ChevronUp, HistoryIcon } from "lucide-react";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 
 interface ProjectSchema{
     _id: string,
@@ -20,6 +22,12 @@ export default function LogsPage():React.JSX.Element{
 
     const[projects, setProjects] =  useState<ProjectSchema[]>([]);
     const[isActive, setIsActive] = useState<string|null>(null);
+
+    const openSignInContext = useContext(OpenSignInContext);
+
+    if(!openSignInContext){
+        throw new Error("OpenSignIn Context must be used within a provider.");
+    };
 
     const fetchProjects = async() => {
         const res : AxiosResponse = await axios.get(`${import.meta.env.VITE_SERVER_URL}/project/allProjects`,{withCredentials: true});
@@ -37,6 +45,7 @@ export default function LogsPage():React.JSX.Element{
                 <main>
                     <Header/>
                     <div className="flex flex-col m-2 md:m-5 border rounded-2xl p-1 md:p-4">
+                        {openSignInContext.openSignIn && <SignInDialog openDialog={openSignInContext.openSignIn} closeDialog={openSignInContext.setOpenSignIn}/>}
                         <h1 className="flex items-center justify-center sticky"><HistoryIcon/>Logs History</h1>
                         <Separator/>
                         <div className="flex flex-col gap-2 md:gap-4 mt-2 overflow-scroll scroll-fade scrollbar-thumb-accent h-[80vh] md:h-[75vh]">
