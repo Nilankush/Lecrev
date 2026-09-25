7f93221f613be9a65bd43d606de1381098664fd9
nilankush <dasnilankush28@gmaail.com>
2026-09-25 19:06:06 +0530

    fix: proxy fix

--- container_runner/Controllers/projectRouterController.ts
+++ container_runner/Controllers/projectRouterController.ts
@@ -74,7 +74,7 @@ export const projectCreateController = async (req: Request, res: Response)=>{
       const newProject = await projectModel.create({
           created_by: req.user.id,
           project_id: project_id,
-          project_url: `https://${project_id}.lecrev-lnf2.onrender.com`
+          project_url: `https://${project_id}~lecrev-lnf2.onrender.com`
       });
       return res.status(201).json(newProject);
   }
